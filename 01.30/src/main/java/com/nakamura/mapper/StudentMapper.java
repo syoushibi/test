5c1f87879712f06679d3324d61bd6a71a7671a9f
syoushibi <894598197@qq.com>
2021-01-30 18:51:02 +0800

    删除了01.30

--- 01.30/src/main/java/com/nakamura/mapper/StudentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.nakamura.mapper;
-
-import java.util.List;
-
-import com.nakamura.bean.Student;
-
-public interface StudentMapper {
-	List<Student> findStudent();
-}
--- 01.30/src/main/java/com/nakamura/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.nakamura.service;
-
-import java.util.List;
-
-import com.nakamura.bean.Student;
-
-public interface StudentService {
-	List<Student> findStudent();
-}
